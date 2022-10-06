ed4df11eb8d64979a1eb3d6127b5b905b9f4dc66
Angel Zhou <angel.zhou1016@gmail.com>
2022-10-06 09:23:35 -0400

    adds text divs dom

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// Buttons
 const rhaenyraBioBtn = document.getElementById('rhaenyra-bio')
 const rhaenyraStatusBtn = document.getElementById('rhaenyra-status')
 const alicentBioBtn = document.getElementById('alicent-bio')
@@ -10,3 +12,11 @@ const laenorBioBtn = document.getElementById('laenor-bio')
 const laenorStatusBtn = document.getElementById('laenor-status')
 const laenaBioBtn = document.getElementById('laena-bio')
 const laenaStatusBioBtn = document.getElementById('laena-statusgit')
+
+// Text divs
+const rhaenyraBioTxt = document.getElementById('rhaenyra-bio-t')
+const alicentBioTxt = document.getElementById('alicent-bio-t')
+const kingBioTxt = document.getElementById('king-bio-t')
+const daemonBioTxt = document.getElementById('daemon-bio-t')
+const laenorBioTxt = document.getElementById('laenor-bio-t')
+const laenaBioTxt = document.getElementById('laena-bio-t')
